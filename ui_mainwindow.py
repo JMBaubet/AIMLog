b73aabc0a57ed3ec0cc997c6ce073b109a533e57
Jean-Marc BAUBET <jean.marc.baubet@gmail.com>
2022-11-23 22:22:04 +0100

    Première ètape de l'IHM

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'designerewGuWK.ui'
+## Form generated from reading UI file 'ui_mainwindowpbRnso.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.4.1
 ##
@@ -15,24 +15,144 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
-    QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QSpacerItem, QStackedWidget,
+    QVBoxLayout, QWidget)
+import ressources_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(1440, 1024)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.widget_2 = QWidget(self.centralwidget)
+        self.widget_2.setObjectName(u"widget_2")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget_2.sizePolicy().hasHeightForWidth())
+        self.widget_2.setSizePolicy(sizePolicy)
+        self.horizontalLayout_2 = QHBoxLayout(self.widget_2)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.menu_vertical = QWidget(self.widget_2)
+        self.menu_vertical.setObjectName(u"menu_vertical")
+        self.verticalLayout_2 = QVBoxLayout(self.menu_vertical)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.pushButton = QPushButton(self.menu_vertical)
+        self.pushButton.setObjectName(u"pushButton")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy1)
+        self.pushButton.setMinimumSize(QSize(22, 0))
+
+        self.verticalLayout_2.addWidget(self.pushButton)
+
+        self.pushButton_2 = QPushButton(self.menu_vertical)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.verticalLayout_2.addWidget(self.pushButton_2)
+
+        self.pushButton_3 = QPushButton(self.menu_vertical)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.verticalLayout_2.addWidget(self.pushButton_3)
+
+        self.pushButton_4 = QPushButton(self.menu_vertical)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+
+        self.verticalLayout_2.addWidget(self.pushButton_4)
+
+        self.verticalSpacer = QSpacerItem(20, 737, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer)
+
+        self.pushButton_5 = QPushButton(self.menu_vertical)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+
+        self.verticalLayout_2.addWidget(self.pushButton_5)
+
+
+        self.horizontalLayout_2.addWidget(self.menu_vertical)
+
+        self.widget_main = QWidget(self.widget_2)
+        self.widget_main.setObjectName(u"widget_main")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.widget_main.sizePolicy().hasHeightForWidth())
+        self.widget_main.setSizePolicy(sizePolicy2)
+        self.verticalLayout_3 = QVBoxLayout(self.widget_main)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.label_4 = QLabel(self.widget_main)
+        self.label_4.setObjectName(u"label_4")
+        font = QFont()
+        font.setPointSize(18)
+        self.label_4.setFont(font)
+        self.label_4.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_3.addWidget(self.label_4)
+
+        self.stackedWidget = QStackedWidget(self.widget_main)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.page_01_analyse = QWidget()
+        self.page_01_analyse.setObjectName(u"page_01_analyse")
+        self.stackedWidget.addWidget(self.page_01_analyse)
+        self.page_03_import_event = QWidget()
+        self.page_03_import_event.setObjectName(u"page_03_import_event")
+        self.stackedWidget.addWidget(self.page_03_import_event)
+        self.page_04_database = QWidget()
+        self.page_04_database.setObjectName(u"page_04_database")
+        self.stackedWidget.addWidget(self.page_04_database)
+        self.page_99_preferences = QWidget()
+        self.page_99_preferences.setObjectName(u"page_99_preferences")
+        self.stackedWidget.addWidget(self.page_99_preferences)
+        self.page_02_import_connexion = QWidget()
+        self.page_02_import_connexion.setObjectName(u"page_02_import_connexion")
+        self.stackedWidget.addWidget(self.page_02_import_connexion)
+
+        self.verticalLayout_3.addWidget(self.stackedWidget)
+
+
+        self.horizontalLayout_2.addWidget(self.widget_main)
+
+
+        self.verticalLayout.addWidget(self.widget_2)
+
+        self._footer = QWidget(self.centralwidget)
+        self._footer.setObjectName(u"_footer")
+        self.horizontalLayout = QHBoxLayout(self._footer)
+        self.horizontalLayout.setSpacing(8)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(20, 0, 0, 0)
+        self.label_3 = QLabel(self._footer)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setPixmap(QPixmap(u":/icons/icons/32px-DGAC_DSNA.png"))
+
+        self.horizontalLayout.addWidget(self.label_3)
+
+        self.label_2 = QLabel(self._footer)
+        self.label_2.setObjectName(u"label_2")
+        sizePolicy2.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy2)
+
+        self.horizontalLayout.addWidget(self.label_2)
+
+        self.label = QLabel(self._footer)
+        self.label.setObjectName(u"label")
+        self.label.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout.addWidget(self.label)
+
+
+        self.verticalLayout.addWidget(self._footer)
+
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(MainWindow)
-        self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 24))
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
 
@@ -41,5 +161,14 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_5.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Base de donn\u00e9e : ATM Domaine, du 19 oct. 2022 au 14 nov. 2022", None))
+        self.label_3.setText("")
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Version : X.Y.Z", None))
     # retranslateUi
 
